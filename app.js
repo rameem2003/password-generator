3860e66f573dff2350f8b304fe2ea8ece6858d8f
rameem2003 <rameem2019@gmail.com>
2022-10-27 13:32:16 +0600

    update ui and function

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const rangeValue = document.getElementById("rangeValue");
 const genBtn = document.getElementById("genBtn");
 const copyBtn = document.getElementById("copyBtn");
 const toast = document.getElementById("toast");
+const copyPass = document.getElementById("copyPass");
 
 let allChar = "abcdefghijklmnopqrstupwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
 
@@ -25,25 +26,27 @@ genBtn.addEventListener("click", generatePassword)
 // slide rangebar
 rangeBar.addEventListener("input", () => {
     rangeValue.innerHTML = rangeBar.value;
-    generatePassword();
+    // generatePassword();
 })
 
 
 // copy button
 copyBtn.addEventListener("click", ()=>{
     navigator.clipboard.writeText(output_window.value);
-    show_toast()
+    show_toast(output_window.value)
 })
 
 
 // show toast massage
 let x;
-function show_toast(){
+function show_toast(password){
+
+    copyPass.innerHTML = password;
 
     clearTimeout(x);
-    toast.style.transform = "translate(550px)"
+    toast.style.transform = "translateY(20px)"
     x = setTimeout(() => {
-        toast.style.transform = "translate(1000px)"
+        toast.style.transform = "translateY(-100px)"
     }, 5000);
 }
 
